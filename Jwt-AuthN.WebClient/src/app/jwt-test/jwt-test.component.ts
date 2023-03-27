cba027e908d20d7ba48b2494fb51e8e44dba3fcf
sabbir_4730 <mdsabbir.onnorokom@gmail.com>
2023-03-27 08:56:43 +0600

    token received and added to header

--- Jwt-AuthN.WebClient/src/app/jwt-test/jwt-test.component.ts
+++ Jwt-AuthN.WebClient/src/app/jwt-test/jwt-test.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JwtTokenService } from '../jwt-token.service';
+import { User } from '../User';
 
 @Component({
   selector: 'app-jwt-test',
   templateUrl: './jwt-test.component.html',
   styleUrls: ['./jwt-test.component.css']
 })
-export class JwtTestComponent {
+export class JwtTestComponent implements OnInit{
+  public user = new User();
+  public token: string = "";
+  public data: string = "";
 
+  constructor(private _service: JwtTokenService) {  }
+  ngOnInit(): void {
+    
+  }
+  Login(){
+    this._service.getToken(this.user).subscribe(data => {this.token = data.toString()});
+    if(this.token.length > 0 ){
+      this._service.getString(this.token).subscribe(data => {console.log(data.toString())});
+    }
+  }
 }
--- Jwt-AuthN.WebClient/src/app/jwt-token.service.ts
+++ Jwt-AuthN.WebClient/src/app/jwt-token.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http'
+import { User } from './User';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,14 @@ export class JwtTokenService {
 
   constructor(private http: HttpClient) { }
 
-  getToken() {
-    let parameter = new HttpParams();
-    //parameter = parameter.append("id", id);
-    return this.http.post("url", { params: parameter });
+  getToken(user:User) {
+    const headers = {'content-type' : 'application/json'};
+    const body = JSON.stringify(user);
+    return this.http.post("https://localhost:7289/security/createToken/", body, {headers:headers});
+  }
+
+  getString(token:string){
+    const headers = {'Authorization': `Bearer ${token}`};
+    return this.http.get<string>("https://localhost:7289/security/getMessage/", {headers: headers});
   }
 }
